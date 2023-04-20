b7fade2a311a3df791ddbe164c89158af2694be8
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-04-20 11:02:52 +0200

    Improve one view

--- data/views.sql
+++ data/views.sql
@@ -26,8 +26,17 @@ AS
 DROP VIEW IF EXISTS employes_number_of_each_client;
 CREATE VIEW employes_number_of_each_client
 AS
-  SELECT id_client, nom_client, COUNT(id_employe) as count_employes
+  SELECT id_client, nom_client, COUNT(id_employe) AS count_employes
   FROM Clients
   JOIN Missions ON (Clients.id_client = Missions.id_client_mission)
   JOIN Contrats USING (id_mission)
-  GROUP BY id_mission;
+  GROUP BY id_mission
+  UNION
+  SELECT id_client, nom_client, 0 AS count_employes
+  FROM Clients
+  WHERE (id_client NOT IN (
+      SELECT id_client_mission
+      FROM Missions
+      JOIN Contrats USING (id_mission)
+  ));
+
